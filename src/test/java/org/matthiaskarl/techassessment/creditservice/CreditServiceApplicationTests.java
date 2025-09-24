0c4677d8106eb5f1f689b8a4ee813a8d60cfd180
Matthias Karl <matt.karl@gmx.at>
2025-09-24 20:24:43 +0200

    initial commit

---
+++ src/test/java/org/matthiaskarl/techassessment/creditservice/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matthiaskarl.techassessment.creditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
